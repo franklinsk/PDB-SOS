8cd98b35e497455e964ea12d0e78931fb9e81725
Franklin Salvatierra <franklinsk@gmail.com>
2015-10-13 23:11:09 +0000

    Changes made by Franklin Salvatierra (franklinsk@gmail.com).

--- PDB-SOS/scripts/app.js
+++ PDB-SOS/scripts/app.js
@@ -4,18 +4,32 @@
     var el = new Everlive(apiKey);
 
     var trackingDataSource = new kendo.data.DataSource({
-                   offlineStorage: "tracking-offline",
         type: "everlive",
         transport: {
             typeName: "ChildTracking"
         },
-                    schema: {
-                        model: {
-                            id: "Id"
-                        }
-                    }
+        schema: {
+            model: {
+                id: "Id"
+            }
+        }
     });
 
+    var offlineDataSource = new kendo.data.DataSource({
+        offlineStorage: "tracking-offline",
+        type: "everlive",
+        transport: {
+            typeName: "ChildTracking"
+        },
+        schema: {
+            model: {
+                id: "Id"
+            }
+        }
+    });
+
+    var childrenDataSource;
+    
     var app; // store a reference to the application object that will be created  later on so that we can use it if need be
 
     window.APP = { // create an object to store the models for each view
@@ -27,24 +41,6 @@
                 title: 'Seguimiento'
             },
             actions: {},
-            contacts: {
-                title: 'Contacts',
-                ds: new kendo.data.DataSource({
-                    data: [{
-                        id: 1,
-                        name: 'Bob'
-                    }, {
-                        id: 2,
-                        name: 'Mary'
-                    }, {
-                        id: 3,
-                        name: 'John'
-                    }]
-                }),
-                alert: function (e) {
-                    alert(e.data.name);
-                }
-            }
         }
     };
 
@@ -52,64 +48,90 @@
         //reasonsForExit: "3",
         addTrackingSubmit: function () {
 
-           if (navigator.onLine) {                               
-   	                trackingDataSource.online(true);
-            }   
-            else
-            {                
-                trackingDataSource.online(false);                                              
-            }
-            
+            if (navigator.onLine) {
+                trackingDataSource.add({
+                    SOSChildID: $('[name="childID"]').val(),
+                    StartDate: $('[name="startDate"]').val(),
+                    EndDate: $('[name="endDate"]').val(),
+                    Phone: $('[name="phone"]').val(),
+                    EmailAddress: $('[name="email"]').val(),
+                    AgeWhenFirstChild: $('[name="ageWhenHasFirstChild"]').val(),
+                    ChildrenNumber: $('[name="childrenNumber"]').val(),
+                    LegalGuardian: $('[name="legalGuardian"]').val(),
+                    SiblingsOutsideSOS: $('[name="siblingsOutsideSOS"]').val(),
+                    HomeType: $('[name="hostageType"]').val(),
+                    HomePlace: $('[name="homePlace"]').val(),
+                    HomeComments: $('[name="hostageComments"]').val(),
+                    HomeImprovementsComments: $('[name="hostageImproveComments"]').val(),
+                    HomeEducationCenterNoSOS: $('[name="educationalCenterNoSOS"]').val(),
+                    CurrentSchoolLevel: $('[name="currentEnrollment"]').val(),
+                    EducationCurrentEnrollment: $('[name="educationCurrentEnrollment"]').val(),
+                    EducationStudyStart: $('[name="educationStudyStart"]').val(),
+                    EducationSpecialityName: $('[name="specialityName"]').val(),
+                    EducationSpecialitySemester: $('[name="specialitySemester"]').val(),
+                    WorkIncomeType: $('[name="sourceOfIncome"]').val(),
+                    WorkType: $('[name="typeOfEmployment"]').val(),
+                    WorkCurrency: $('[name="workCurrency"]').val(),
+                    WorkSpeacialityRelated: $('[name="workRelatedWithSpeciality"]').val(),
+                    WorkSector: $('[name="areaOfWork"]').val(),
+                    WorkMonthsContinuity: $('[name="continueWorkinMonths"]').val(),
+                    WorkMonthlyIncome: $('[name="incomeMonthly"]').val(),
+                    WorkMonthsUnemployed: $('[name="monthsUnemployee"]').val(),
+                    HealthHasDisabilities: $('[name="hasDisability"]').val(),
+                    HealthHowDisabilityAffects: $('[name="affectsInDailyLife"]').val(),
+                    HealthDisabilityComments: $('[name="commentsAboutHandicap"]').val()
+                });
+                trackingDataSource.sync();
 
-            trackingDataSource.add({
-                SOSChildID: $('[name="childID"]').val(),
-                StartDate: $('[name="startDate"]').val(),
-                EndDate: $('[name="endDate"]').val(),
-                Phone: $('[name="phone"]').val(),
-                EmailAddress: $('[name="email"]').val(),
-                AgeWhenFirstChild: $('[name="ageWhenHasFirstChild"]').val(),
-                ChildrenNumber: $('[name="childrenNumber"]').val(),
-                LegalGuardian: $('[name="legalGuardian"]').val(),
-                SiblingsOutsideSOS: $('[name="siblingsOutsideSOS"]').val(),
-                HomeType: $('[name="hostageType"]').val(),
-                HomePlace: $('[name="homePlace"]').val(),
-                HomeComments: $('[name="hostageComments"]').val(),
-                HomeImprovementsComments: $('[name="hostageImproveComments"]').val(),
-                HomeEducationCenterNoSOS: $('[name="educationalCenterNoSOS"]').val(),
-                CurrentSchoolLevel: $('[name="currentEnrollment"]').val(),
-                EducationCurrentEnrollment: $('[name="educationCurrentEnrollment"]').val(),
-                EducationStudyStart: $('[name="educationStudyStart"]').val(),
-                EducationSpecialityName: $('[name="specialityName"]').val(),
-                EducationSpecialitySemester: $('[name="specialitySemester"]').val(),
-                WorkIncomeType: $('[name="sourceOfIncome"]').val(),
-                WorkType: $('[name="typeOfEmployment"]').val(),
-                WorkCurrency: $('[name="workCurrency"]').val(),
-                WorkSpeacialityRelated: $('[name="workRelatedWithSpeciality"]').val(),
-                WorkSector: $('[name="areaOfWork"]').val(),
-                WorkMonthsContinuity: $('[name="continueWorkinMonths"]').val(),
-                WorkMonthlyIncome: $('[name="incomeMonthly"]').val(),
-                WorkMonthsUnemployed: $('[name="monthsUnemployee"]').val(),
-                HealthHasDisabilities: $('[name="hasDisability"]').val(),
-                HealthHowDisabilityAffects: $('[name="affectsInDailyLife"]').val(),
-                HealthDisabilityComments: $('[name="commentsAboutHandicap"]').val()
-            });
-            trackingDataSource.sync();
-
-           if (navigator.onLine) {                               
                 navigator.notification.alert("Se ha registrado correctamente");
-            }   
-            else
-            {                
-                navigator.notification.alert("Se ha registrado correctamente en modo desconectado");   
-            }            
-            
+
+            } else {
+                offlineDataSource.online(false);
+
+
+                offlineDataSource.add({
+                    SOSChildID: $('[name="childID"]').val(),
+                    StartDate: $('[name="startDate"]').val(),
+                    EndDate: $('[name="endDate"]').val(),
+                    Phone: $('[name="phone"]').val(),
+                    EmailAddress: $('[name="email"]').val(),
+                    AgeWhenFirstChild: $('[name="ageWhenHasFirstChild"]').val(),
+                    ChildrenNumber: $('[name="childrenNumber"]').val(),
+                    LegalGuardian: $('[name="legalGuardian"]').val(),
+                    SiblingsOutsideSOS: $('[name="siblingsOutsideSOS"]').val(),
+                    HomeType: $('[name="hostageType"]').val(),
+                    HomePlace: $('[name="homePlace"]').val(),
+                    HomeComments: $('[name="hostageComments"]').val(),
+                    HomeImprovementsComments: $('[name="hostageImproveComments"]').val(),
+                    HomeEducationCenterNoSOS: $('[name="educationalCenterNoSOS"]').val(),
+                    CurrentSchoolLevel: $('[name="currentEnrollment"]').val(),
+                    EducationCurrentEnrollment: $('[name="educationCurrentEnrollment"]').val(),
+                    EducationStudyStart: $('[name="educationStudyStart"]').val(),
+                    EducationSpecialityName: $('[name="specialityName"]').val(),
+                    EducationSpecialitySemester: $('[name="specialitySemester"]').val(),
+                    WorkIncomeType: $('[name="sourceOfIncome"]').val(),
+                    WorkType: $('[name="typeOfEmployment"]').val(),
+                    WorkCurrency: $('[name="workCurrency"]').val(),
+                    WorkSpeacialityRelated: $('[name="workRelatedWithSpeciality"]').val(),
+                    WorkSector: $('[name="areaOfWork"]').val(),
+                    WorkMonthsContinuity: $('[name="continueWorkinMonths"]').val(),
+                    WorkMonthlyIncome: $('[name="incomeMonthly"]').val(),
+                    WorkMonthsUnemployed: $('[name="monthsUnemployee"]').val(),
+                    HealthHasDisabilities: $('[name="hasDisability"]').val(),
+                    HealthHowDisabilityAffects: $('[name="affectsInDailyLife"]').val(),
+                    HealthDisabilityComments: $('[name="commentsAboutHandicap"]').val()
+                });
+                offlineDataSource.sync();
+
+                navigator.notification.alert("Se ha registrado correctamente en modo desconectado");
+            }
 
         }
     });
 
     window.APP.models.tracking = kendo.observable({
         submit: function () {
-            var childrenDataSource = new kendo.data.DataSource({
+            childrenDataSource = new kendo.data.DataSource({
                 type: "everlive",
                 transport: {
                     typeName: "Child"
@@ -146,64 +168,60 @@
     window.APP.synchro = kendo.observable({
         submit: function () {
 
-                var offlineDataSource = new kendo.data.DataSource({
-                   offlineStorage: "tracking-offline",
-        type: "everlive",
-        transport: {
-            typeName: "ChildTracking"
-        },
+
+
+            if (navigator.onLine) {
+
+                var sLocalStorage = localStorage.getItem("tracking-offline");
+                var jLocalStorage = JSON.parse(sLocalStorage);
+
+                var localStorageDataSource = new kendo.data.DataSource({
+                    data: jLocalStorage
+                });
+
+                $("#listView").kendoMobileListView({
+                    dataSource: localStorageDataSource,
+                    template: "#: StartDate # - #: EndDate # - #: SOSChildID #"
+                });
+
+
+                var synchroDataSource = new kendo.data.DataSource({
+                    type: "everlive",
+                    transport: {
+                        typeName: "ChildTracking"
+                    },
                     schema: {
                         model: {
                             id: "Id"
                         }
                     }
-    });
-            
-            if (navigator.onLine) {                               
+                });
 
-         var sLocalStorage = localStorage.getItem("tracking-offline");
-         var jLocalStorage = JSON.parse(sLocalStorage);
-               
-               var OfflineDataSource = new kendo.data.DataSource({
-                data: jLocalStorage 
-            });
+                for (var item in jLocalStorage) {
 
-            $("#listView").kendoMobileListView({
-                dataSource: OfflineDataSource,
-                template: "#: StartDate # - #: EndDate # - #: SOSChildID #"
-            });
-               
 
-               offlineDataSource.online(true);
-               
-            for (var item in jLocalStorage) {
 
-                
-                
-                    offlineDataSource.add({
+                    synchroDataSource.add({
                         SOSChildID: jLocalStorage[item]["SOSChildID"],
                         StartDate: jLocalStorage[item]["StartDate"],
                         EndDate: jLocalStorage[item]["EndDate"]
                     });
 
-            }
-               
+                }
+
                 localStorage.removeItem("tracking-offline");
-               offlineDataSource.sync();
+                synchroDataSource.sync();
 
 
-               
-                                               navigator.notification.alert("Sincronizacion finalizada!!!");   
-               
-            }   
-            else
-            {                
-                navigator.notification.alert("No se ha detectado una conexion activa a internet");   
-            }            
+                navigator.notification.alert("Sincronizacion finalizada!!!");
+
+            } else {
+                navigator.notification.alert("No se ha detectado una conexion activa a internet");
+            }
 
         }
-    });    
-    
+    });
+
     // this function is called by Cordova when the application is loaded by the device
     document.addEventListener('deviceready', function () {
         navigator.splashscreen.hide(); // hide the splash screen as soon as the app is ready. otherwise  Cordova will wait 5 very long seconds to do it for you.
@@ -213,4 +231,5 @@
             initial: 'views/home.html' // the application needs to know which view to load first
         });
     }, false);
+    offlineDataSource.online(false);
 }());
